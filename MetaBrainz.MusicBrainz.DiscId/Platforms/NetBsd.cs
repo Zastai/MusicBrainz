6662b91e75df9332db262f0e6073eb1b68ed5830
Tim Van Holder <tim.vanholder@gmail.com>
2020-03-22 18:47:01 +0100

    Removed everything except the README.
    All projects have been split off in their own repositories.

--- MetaBrainz.MusicBrainz.DiscId/Platforms/NetBsd.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
-
-  internal sealed class NetBsd : NetOrOpenBsd {
-
-    // Currently just a placeholder for any functionality specific to NetBSD
-
-  }
-
-}
--- MetaBrainz.MusicBrainz.DiscId/Platforms/OpenBsd.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
-
-  internal sealed class OpenBsd : NetOrOpenBsd {
-
-    // Currently just a placeholder for any functionality specific to OpenBSD
-
-  }
-
-}
